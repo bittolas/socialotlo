a2719a55e6ecd05a63bf91bc0aaa9fc57ab7b8da
bittolas <80890990+bittolas@users.noreply.github.com>
2021-12-28 23:02:18 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -432,3 +432,17 @@ wss.on('connection', (ws) => {
 
   });
 })
+
+// SQL SERVER
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+  client.end();
